33b7eb7845bf57a75c1a8d0f562f05bf54f1b0fa
Xeon <nickandreyuser@gmail.com>
2019-12-01 14:09:21 +0100

    update db.json, add fetch data from db

--- src/js/get-goods.js
+++ src/js/get-goods.js
@@ -1,3 +1,20 @@
 (function() {
+  fetch(`https://my-json-server.typicode.com/nickAndrey/vuesax-ecommerce/goods`)
+    .then((resp) => resp.json())
+    .then((data) => renderProduct(data))
+    .catch((err) => console.log(err));
 
+  const renderProduct = (data) => {
+    const template = document.querySelector('#product');
+    Object.entries(data).map(([key, value]) => {
+      const clone = template.content.cloneNode(true);
+      const productCard = clone.querySelector('.product');
+      const cloneTo = document.querySelector('.goods');
+      const productImg = productCard.querySelector('.product__img');
+
+      productImg.src = value.src;
+      productImg.alt = value.title;
+      cloneTo.append(productCard);
+    });
+  };
 })();
